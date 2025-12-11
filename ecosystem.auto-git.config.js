f575778505669062a1446f9e97d728ed6f08b8f2
oven-ttta <sitthisak2007.1@gmail.com>
2025-12-11 01:36:50 +0000

    Auto commit: 2025-12-11 01:36:50 - 1 files changed

--- ecosystem.auto-git.config.js
+++ ecosystem.auto-git.config.js
@@ -1,16 +1,16 @@
-{
-  "apps": [
+module.exports = {
+  apps: [
     {
-      "name": "auto-git-push",
-      "script": "/root/MC-Server/auto_git_push_daemon.sh",
-      "cwd": "/root/MC-Server",
-      "interpreter": "/bin/bash",
-      "autorestart": true,
-      "watch": false,
-      "max_memory_restart": "100M",
-      "error_file": "/root/MC-Server/logs/auto-git-push-error.log",
-      "out_file": "/root/MC-Server/logs/auto-git-push-out.log",
-      "log_date_format": "YYYY-MM-DD HH:mm:ss"
+      name: 'auto-git-push',
+      script: '/root/MC-Server/auto_git_push_daemon.sh',
+      cwd: '/root/MC-Server',
+      interpreter: '/bin/bash',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '9999M',
+      error_file: '/root/MC-Server/logs/auto-git-push-error.log',
+      out_file: '/root/MC-Server/logs/auto-git-push-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss'
     }
   ]
-}
+};
